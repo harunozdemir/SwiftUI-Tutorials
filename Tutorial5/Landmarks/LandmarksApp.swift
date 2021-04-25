47f7388d233790484902ce7427fb92603fff16f4
Harun Özdemir <harun.ozdemir@consultant.turkcell.com.tr>
2021-04-26 00:48:42 +0300

    first commit

---
+++ Tutorial5/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Harun on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
---
+++ Tutorial5/README.md
@@ -0,0 +1,6 @@
+# SwiftUI-Tutorial4
+Source -> https://developer.apple.com/tutorials/swiftui/animating-views-and-transitions
+
+<p float="left">
+  <img src="https://github.com/harunozdemir/SwiftUI-Tutorials/blob/main/Tutorial5/Landmarks/Images/output.png" width="315">
+</p>
